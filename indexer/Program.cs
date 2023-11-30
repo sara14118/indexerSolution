df4f772163aee09ab7af37824df6c2b5008cfc50
sara <sara.alzadjali14@outlook.com>
2023-11-30 12:26:39 +0400

    Add project files.

---
+++ indexer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //create instance of program2 class 
+
+            Program2 obj = new Program2();
+
+            //insert values in obj[] using indexer position 
+            obj[0] = "sara";
+            obj[1] = "ahmed";
+            obj[2] = "khlood";
+
+            Console.WriteLine($"First element in obj :{obj[0]}");
+            Console.WriteLine($"second element in obj :{obj[1]}");
+
+        }
+    }
+}
---
+++ indexer/Program2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexer
+{
+    internal class Program2
+    {
+
+        // declare an array to store element
+        private string[] studntName = new string[10];
+
+        // define an indexer 
+
+        public string this[int index]
+        {
+
+            get
+            {
+                //return value of stored at studentName array
+                return studntName[index];
+
+            }
+            set
+            {
+                //assigns value to studentName
+                studntName[index] = value;
+            }
+        }
+
+
+
+
+
+
+       
+    }
+}
